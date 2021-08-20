c5ed7f3f83cff0301c1f2ee37f68f4fe0f42d4c6
ThiloU <65457019+ThiloU@users.noreply.github.com>
2021-08-20 15:01:24 +0200

    Update system-cpu.sh

--- .config/polybar/polybar-scripts/system-cpu.sh
+++ .config/polybar/polybar-scripts/system-cpu.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # echo "︁ $[100-$(vmstat 1 2|tail -1|awk '{print $15}')]%  $(sensors coretemp-isa-0000| grep -A 0 'Package id 0' | cut -c17-18)°C"
-echo " $[100-$(vmstat 1 2|tail -1|awk '{print $15}')]%  $(sensors coretemp-isa-0000| grep -A 0 'Package id 0' | cut -c17-18)°C"
+echo "$[100-$(vmstat 1 2|tail -1|awk '{print $15}')]%  $(sensors coretemp-isa-0000| grep -A 0 'Package id 0' | cut -c17-18)°C"
