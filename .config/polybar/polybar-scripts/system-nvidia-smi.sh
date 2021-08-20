0aec0e3ef1c67a9b4355283a4d81b784ac1082ed
ThiloU <65457019+ThiloU@users.noreply.github.com>
2021-08-20 15:02:01 +0200

    Update system-nvidia-smi.sh

--- .config/polybar/polybar-scripts/system-nvidia-smi.sh
+++ .config/polybar/polybar-scripts/system-nvidia-smi.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-echo " $(nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits)%  $(nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader,nounits)°C"
+echo "$(nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits)%  $(nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader,nounits)°C"
